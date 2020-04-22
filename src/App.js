6a8b5e95f42ac1604ca6ab8084e471007c3549bb
ilkyun <iik0475@gmail.com>
2020-04-22 14:05:01 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "https://en.wikipedia.org/wiki/Kimchi#/media/File:Various_kimchi.jpg",
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: "Cutlet",
-    image: "https://en.wikipedia.org/wiki/Cutlet#/media/File:Escalope2.jpg",
-    rating: 3,
-  },
-  {
-    id: 3,
-    name: "Chicken",
-    image:
-      "https://en.wikipedia.org/wiki/Fried_chicken#/media/File:Fried-Chicken-Set.jpg",
-    rating: 4,
-  },
-  {
-    id: 4,
-    name: "Hamburger",
-    image:
-      "https://makeyourmeals.com/wp-content/uploads/2019/06/grilled-hamburger-1.jpg",
-    rating: 2,
-  },
-];
-function Food({ name, picture, rating }) {
-  return (
-    <div>
-      <h1>I like {name}</h1>
-      <h4>Rating: {rating}/5</h4>
-      <img src={picture} alt={name} />
-    </div>
-  );
-}
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+  }
+  render() {
+    const { isLoading } = this.state;
 
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          picture={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+    return <div>{isLoading ? "Loading...." : "We are ready"}</div>;
+  }
 }
 
 export default App;
