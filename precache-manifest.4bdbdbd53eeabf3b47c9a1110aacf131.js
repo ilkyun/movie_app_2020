f82b0df297630039eb33fd4c9e8d255995dc43a6
ilkyun <iik0475@gmail.com>
2020-04-22 15:24:10 +0900

    Updates

---
+++ precache-manifest.4bdbdbd53eeabf3b47c9a1110aacf131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a2314bc985c2725b9dbae6473c98e4",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "39ead9fbf084bcdbfa78",
+    "url": "/movie_app_2020/static/css/main.996faeb3.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ead9fbf084bcdbfa78",
+    "url": "/movie_app_2020/static/js/main.3eaf69e2.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
